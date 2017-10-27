ee957ed74883204b56aaf9a2987c6e7c1e1028e4
johnErbs <31472353+johnErbs@users.noreply.github.com>
2017-10-27 10:20:08 +0200

    Pushed

---
+++ BudgetManager2017/BudgetManager2017/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BudgetManager2017.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BudgetManager2017/BudgetManager2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BudgetManager2017.WebApiApplication" Language="C#" %>
