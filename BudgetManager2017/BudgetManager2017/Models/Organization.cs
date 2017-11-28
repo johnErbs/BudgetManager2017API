aa6cd7a9110515c50ee16ff118dc2c0402fad6de
LheesyCovi <31472269+LheesyCovi@users.noreply.github.com>
2017-11-28 10:58:54 +0100

    Organization class created

---
+++ BudgetManager2017/BudgetManager2017/Models/Organization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetManager2017.Models
+{
+    public class Organization
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+    }
+}
